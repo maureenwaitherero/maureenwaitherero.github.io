413d818df15e40081d025d689bd705ecfda25910
maureenwaitherero <waithereromaureen@gmail.com>
2016-05-06 11:40:11 +0300

    changing form

--- js/index.js
+++ js/index.js
@@ -84,7 +84,7 @@ $contactForm.submit(function(e) {
         },
         success: function(data) {
             $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+            $contactForm.append('thankyou.html');
         },
         error: function(err) {
             $contactForm.find('.alert--loading').hide();
