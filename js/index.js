f70a123c269b5a680373854f7eb6256ed9c246bf
maureenwaitherero <waithereromaureen@gmail.com>
2016-05-06 09:59:30 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -70,12 +70,28 @@ $(function() {
             });
       	
 
-
-
-
-
-
-
+/*formspree ajax*/
+var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: '//formspree.io/waithereromaureen@gmail.com',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+            $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+        },
+        success: function(data) {
+            $contactForm.find('.alert--loading').hide();
+            $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+        },
+        error: function(err) {
+            $contactForm.find('.alert--loading').hide();
+            $contactForm.append('<div class="alert alert--error">Oops, there was an error send error.</div>');
+        }
+    });
+});
 
 
 
